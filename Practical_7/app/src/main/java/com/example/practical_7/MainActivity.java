ad4f7cb3a293c4aca69223be1b0217e0a3be707e
Saurabh <saurabhsalunke91@gmail.com>
2023-05-20 16:57:52 +0530

    Assignment7

---
+++ Practical_7/app/src/main/java/com/example/practical_7/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.practical_7;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private TextView batteryPercentage;
+    private BroadcastReceiver batteryReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        batteryPercentage = findViewById(R.id.batteryPercentage);
+
+        batteryReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                progressBar.setProgress(level);
+                batteryPercentage.setText(level + "%");
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(batteryReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(batteryReceiver);
+    }
+}
