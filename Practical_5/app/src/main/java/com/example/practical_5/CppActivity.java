74ba6e229a7d97757f67e564745bbb91d56d4925
Soham <soham0005>
2023-05-20 12:23:22 +0530

    Practical 5

---
+++ Practical_5/app/src/main/java/com/example/practical_5/CppActivity.java
@@ -0,0 +1,14 @@
+package com.example.practical_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cpp);
+    }
+}
---
+++ Practical_5/app/src/main/java/com/example/practical_5/JavaActivity.java
@@ -0,0 +1,18 @@
+package com.example.practical_5;
+
+import static com.example.practical_5.R.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class JavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_java);
+    }
+}
